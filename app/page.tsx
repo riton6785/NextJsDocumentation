87165b064469b48819d85efee3f9086a39c352b0
Aditya <sharmaaditya6785@gmail.com>
2025-03-27 23:50:28 +0530

    Learned how to use the css using module using global css and the clsx libray. Learned how to use the font Learned how to use the Image Learned about the page routing and use of page.tsx(as it reates a route corresponding to paret folder) Learned about the LINK compoenet to work between routes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Styles from'@/app/ui/test.module.css';
 
-import { lusitana } from '@/app/ui/font';
+import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -4,4 +4,4 @@ export const inter = Inter({ subsets: ['latin'] });
 
 import { Lusitana } from 'next/font/google';
 
-export const lusitana = Lusitana({weight: ['400', '700']})
+export const lusitana = Lusitana({weight: ['400', '700'], subsets: ['latin']})
